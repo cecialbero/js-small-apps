8f424cf112858a5b6e260f76841fc526b6fe9e75
Cecilia Albero <calbero@itx.com>
2018-04-14 18:14:04 +0200

    Set the releaseKey function

--- 01-DrumKit/js/custom.js
+++ 01-DrumKit/js/custom.js
@@ -1,13 +1,19 @@
-window.addEventListener('keydown', playSound);
-
 function playSound(e) {
     var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; //stop the function if no audio
     audio.currentTime = 0; //reset to 0 the current time
     audio.play();
-    key.classList.add('play');
-    audio.onended = function() {
-        alert("The audio has ended");
-    };
+    key.classList.add('playing');
 }
+
+function releaseKey(e) {
+  var keys = document.querySelectorAll('.key');
+  keys.forEach(function(key){
+    key.classList.remove('playing');
+  });
+}
+
+window.addEventListener('keydown', playSound);
+
+window.addEventListener('keyup', releaseKey);
